8c3276a0648fd8ec9a5e2b63eefe66a0731436f0
princy <princy21aj@gmail.com>
2026-01-21 00:46:51 +0530

    variables

---
+++ 01_python_basics/variables.py
@@ -0,0 +1,19 @@
+#VARIABLES is a name used to store dat in memory.
+
+
+user_name = "princy"           # string
+year =2026                     # int
+pi = 3.14                      # float
+is_student = True              # bool
+
+#sample
+
+student_name ="princy"
+total_marks = 527
+
+# multiple assignment
+x , y , z = 36 , 69 , 93 
+
+# same value assignment
+x = y = z = 369
+
