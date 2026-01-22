5bb205e5ce6964063bf8a9929067fdd34c15c508
princy <princy21aj@gmail.com>
2026-01-22 19:41:20 +0530

    Day 1: update variables example with output

--- 01_python_basics/variables.py
+++ 01_python_basics/variables.py
@@ -6,14 +6,20 @@ year =2026                     # int
 pi = 3.14                      # float
 is_student = True              # bool
 
+print(f"I am {user_name}")
+print(f"wish this {year} year would be an great learnig path")
+print(f"the constant value of pi is {pi}")
+print (f"he was an admin of this group{is_student}")
 #sample
 
 student_name ="princy"
 total_marks = 527
+print(f"the name of the student {student_name}")
+print(f"the total marks of the student {total_marks}")
+
 
 # multiple assignment
 x , y , z = 36 , 69 , 93 
 
 # same value assignment
-x = y = z = 369
 
