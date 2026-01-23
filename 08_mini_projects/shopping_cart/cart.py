3e92eaed4c6066a91e2c250d3a5df9ee02c2fc77
princy <princy21aj@gmail.com>
2026-01-23 07:35:23 +0530

    Add shopping cart mini project

--- 08_mini_projects/shopping_cart/cart.py
+++ 08_mini_projects/shopping_cart/cart.py
@@ -1,10 +1,12 @@
-product_item_name=str(input("Enter the item u bought:"))
+# shopping cart
+
+product_item_name=(input("Enter the item u bought:"))
 price=int(input("Enter the price of the item:"))
 quantity=int(input("Enter the quantity of the item:"))
+
 total=price*quantity
+
 print(f"You like to have {product_item_name}")
 print(f"this is the price of this item ${price}")
 print(f"You ordered {quantity} x {product_item_name}")
 print(f"your total price is {total}")
-
-
