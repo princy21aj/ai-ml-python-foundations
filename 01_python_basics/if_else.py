e375760943f84698da28209883c3529199ca29b0
princy <princy21aj@gmail.com>
2026-01-23 23:52:26 +0530

    day 2: operators and conditional statements

---
+++ 01_python_basics/if_else.py
@@ -0,0 +1,34 @@
+# Even or odd
+
+num = 6
+
+if num % 2 ==0:
+    print("The Number is even")
+else:
+    print("The Number is odd")
+
+# Grade system
+
+Marks = 85
+
+if Marks >= 90:
+    print("Your Grade is : A")
+elif Marks >= 85:
+    print("Your Grade is : B")
+elif marks >= 60:
+    print("Your Grade is : C")
+elif Marks >= 50:
+    print("Your Grade is : D")
+else:
+    print("Your fail")
+
+# Nested_if example
+
+age = 17
+if age >= 18:
+    if age >= 21:
+        print("your eligible for everything")
+    else:
+        print("your an adult but with limited access")
+else:
+    print("you are a minor")
---
+++ 01_python_basics/operators.py
@@ -0,0 +1,27 @@
+# operators
+# arithmetic operators
+
+num_1 = 3
+num_2 = 6
+
+print(f"num_1 {num_1} and num_2 {num_2}" )
+print("Addition:",num_1 + num_2)
+print("subtraction:",num_1 - num_2)
+print("Multiplication:", num_1 * num_2)
+print("Division:",num_1 / num_2)
+print("Modulus:",num_1 % num_2)
+
+# comparison operators
+
+print("num_1 < num_2",num_1 < num_2)
+print("num_1 > num_2",num_1 > num_2)
+print("num_1 == num_2",num_1 == num_2)
+
+# logical operators
+
+age = 18
+has_id = True
+
+print("eligible for voting:",age<=18 and has_id)
+print("not eligible for votind",age>=18 or not has_id)
+
